44cfdca8d3701e7d16b29c67fba24684e11d2457
fanere <fanere@deloitte.de>
2025-11-20 11:39:35 +0100

    chore: remove experimental appDir configuration from Next.js config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     AUDIO_SERVICE_URL: process.env.AUDIO_SERVICE_URL || 'http://localhost:8000',
   },
